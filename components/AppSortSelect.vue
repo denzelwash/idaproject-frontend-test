6ce5f82fbcbdb3c77d96bc5f7ef76901161c5658
Denis Obolevich <denzelwash33@mail.ru>
2022-09-20 15:54:45 +0300

    Добавил верстку селекта

---
+++ components/AppSortSelect.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="form-group mb-0">
+    <select class="form-group__select">
+      <option value="default" selected disabled>По умолчанию</option>
+      <option value="min">По цене min</option>
+      <option value="max">По цене max</option>
+      <option value="name">По наименованию</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,12 @@
   <div class="container">
     <div class="add-product">
       <AppNewProduct></AppNewProduct>
-      <div class="products"></div>
+      <div class="products">
+        <div class="products__top">
+          <AppSortSelect></AppSortSelect>
+        </div>
+        <div class="products__grid"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,5 +24,18 @@ export default {
   grid-template-columns: 332px 1fr;
   gap: 1rem;
   padding-top: 2rem;
+}
+
+.products {
+  &__top {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 16px;
+  }
+  &__grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 16px;
+  }
 }
 </style>
